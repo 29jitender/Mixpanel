421320c1a41a16b3e600c8359e42626c044b48e2
jitender chaudhary <29jitender@gmail.com>
2013-06-18 22:28:13 +0530

    Revert "edit conf. setting"
    This reverts commit 7ebe44e7ea2662d27583268267cca5142ee5b3b2.

--- src/com/mixpanel/src/Conf_API.java
+++ src/com/mixpanel/src/Conf_API.java
@@ -1,9 +1,8 @@
 package com.mixpanel.src;
 
 public class Conf_API {
-	
-	static final String API_sceret= "enter your api sceret";//defining variable 
-	  static final String API_key="enter your api key";
-	  static final String expire="enter your expire";
+	static final String API_sceret= "85bf3287d651827687a17d0e45939650";//defining variable 
+	  static final String API_key="3e7cd6659f6a847eaf6bbccdbe20ea19";
+	  static final String expire="1410949530";
 
 }
