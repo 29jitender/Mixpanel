15a8ea4b380715e6c7638963cac938dc36086601
jitender chaudhary <29jitender@gmail.com>
2013-06-18 22:35:40 +0530

    deleted file

--- src/com/mixpanel/src/Conf_API.java
+++
@@ -1,8 +0,0 @@
-package com.mixpanel.src;
-
-public class Conf_API {
-	static final String API_sceret= "85bf3287d651827687a17d0e45939650";//defining variable 
-	  static final String API_key="3e7cd6659f6a847eaf6bbccdbe20ea19";
-	  static final String expire="1410949530";
-
-}
