5262d9ff431ae75df06c04b6124241f166352a59
29jitender <29jitender@gmail.com>
2013-06-18 22:38:45 +0530

    Create configuration file

---
+++ src/com/mixpanel/src/Conf_API.java
@@ -0,0 +1,8 @@
+package com.mixpanel.src;
+
+public class Conf_API {
+  static final String API_sceret= "enter API secret";//defining variable 
+	  static final String API_key="enter API key";
+	  static final String expire="enter Expire";
+
+}
