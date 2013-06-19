21f8498fcd747666d2231b0bae2f97c6aabc6cf2
jitender chaudhary <29jitender@gmail.com>
2013-06-19 17:39:13 +0530

    fixed untracked files

--- src/com/mixpanel/src/Conf_API.java
+++
@@ -1,8 +0,0 @@
-package com.mixpanel.src;
-
-public class Conf_API {
-  static final String API_sceret= "your_api_secret_here";//defining variable 
-	  static final String API_key="your_api_key_here";
-	  static final String expire="1410949530";
-
-}
