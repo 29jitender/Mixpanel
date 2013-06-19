48b6e702adf5cda62aaf91e5b219bc4027e96a98
jitender chaudhary <29jitender@gmail.com>
2013-06-19 17:34:21 +0530

    updated conf

--- src/com/mixpanel/src/Conf_API.java
+++ src/com/mixpanel/src/Conf_API.java
@@ -1,8 +1,8 @@
 package com.mixpanel.src;
 
 public class Conf_API {
-  static final String API_sceret= "85bf3287d651827687a17d0e45939650";//defining variable 
-	  static final String API_key="3e7cd6659f6a847eaf6bbccdbe20ea19";
+  static final String API_sceret= "your_api_secret_here";//defining variable 
+	  static final String API_key="your_api_key_here";
 	  static final String expire="1410949530";
 
 }
